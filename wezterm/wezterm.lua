57832fdf0131a3f713b60756d34bd84551dc73b8
Huang Yiming <hlstwizard@outlook.com>
2026-03-03 19:33:10 +0800

    Add wezterm shortcuts; add context7 MCP

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,6 +10,16 @@ config.keys = {
     mods = 'CMD',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
+  {
+    key = 'd',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
 }
 
 return config
