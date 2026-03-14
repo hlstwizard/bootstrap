d48463c5a8153c753aabb40ea74d7308eb80bb10
hlstwizard <h.l.s.t@163.com>
2026-03-14 17:01:58 +0800

    Update wezterm leader key

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,25 +1,28 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.leader = { key = "\\", mods = "CTRL", timeout_milliseconds = 1000 }
+
 -- Basic: split window like iTerm (Cmd+d)
 config.keys = {
-  {
-    key = 'd',
-    mods = 'CMD',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'd',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'w',
-    mods = 'CMD',
-    action = wezterm.action.CloseCurrentPane { confirm = false },
-  },
+	{ mods = "LEADER", key = "p", action = wezterm.action.PaneSelect },
+	{
+		key = "d",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
 }
 
 return config
